74fe1687eb5e1fb215abac897ddfdc303217c504
Zuzana Sebechlebská <zuzanasebechlebska@gmail.com>
2022-11-21 13:04:21 +0100

    parsing test

---
+++ tests/test_get_variables.py
@@ -0,0 +1,15 @@
+import logigram.visualisation as visualisation
+from parameterized import parameterized
+
+import unittest
+import  filecmp
+
+class KnownFigures(unittest.TestCase):
+    @parameterized.expand([("A{1}<=>F",False,True,["A"]),
+                           ("A{1}*B{2}+A{2}<=>F",False,True,["A","B"]),
+                           ("A*b=F",False,False,["A","B"]),
+                           ("A=F",False,False,["A"])
+    ])
+    def test_get_variables(self, input_f,multi_output,multi_value, variables):
+        self.assertEqual(visualisation.get_the_variabels(input_f,multi_output,
+                                                        multi_value), variables)
